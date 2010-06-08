c80272cd44881c460cde3685de4c40c811a9d743
Daisuke Ogihara <dogihara@gmail.com>
2010-06-09 00:40:53 +0900

    first commit

---
+++ js/daimaruonline.js
@@ -0,0 +1,22 @@
+﻿$(function() {
+
+  $('a').hover(
+    function() {
+      $(this).css('color', 'red');
+    },
+    function() {
+      $(this).css('color', 'black'); 
+    }
+  );
+
+  $('p.click').toggle(
+    function() {
+      $('dl').slideUp(1000);
+    },
+    function() {
+      $('dl').slideDown(1000);
+    }
+  );
+
+  $('p.draggable').draggable();
+});
